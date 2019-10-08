99e895c16b70feb8c2363b4a2536b1404b32647a
Damion <damion.lance@gmail.com>
2019-10-08 14:30:38 -0400

    Further work on correcting recursion

--- netSolver.py
+++ netSolver.py
@@ -1,14 +1,16 @@
 def solve_hedron(hedron):
-    return num_children(hedron, 1, [1])
+    return num_children(hedron, [1])
 
-def num_children(hedron, face, parents):
+def num_children(hedron, faces):
     total = 0
     num_unique_children = 0
-    for child in hedron[face]:
-        if child not in parents:
-            num_unique_children += 1
-            parents.append(child)
-            total += num_children(hedron, child, parents)
+    for face in faces:
+        for child in hedron[face]:
+            if child not in faces:
+                num_unique_children += 1
+                faces_temp = faces.copy()
+                faces_temp.append(child)
+                total += num_children(hedron, faces_temp)
     if num_unique_children == 0:
         return 1
     else:
@@ -17,3 +19,4 @@ def num_children(hedron, face, parents):
 
 hedron = {1: [2, 3, 4, 5], 2: [3, 1, 4, 6], 3: [1, 2, 5, 6], 4: [1, 2, 6, 5], 5: [1, 3, 6, 4], 6: [2, 3, 4, 5]}
 print(solve_hedron(hedron))
+
